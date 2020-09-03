6324b71eadd07745b1d3ccce8d626e2e9956a68e
Betty <1942433767@qq.com>
2020-09-03 10:10:51 +0800

    feat(Resource): add mulobject type

---
+++ src/components/MulObjectEditor.vue
@@ -0,0 +1,153 @@
+<template>
+  <!-- eslint-disable -->
+  <el-table class="mul-object-editor" :data="tableData" :render-header="labelHeads" size="mini">
+    <el-table-column :label="item" v-for="(item, index) in headers" :key="index">
+      <template slot-scope="{ row }">
+        {{ row[item].type }}
+        <emq-select
+          v-if="row[item].type === 'emq-select'"
+          v-bind="row[item].$attrs"
+          v-model="row[row[item].key]"
+          class="el-select--public"
+          popper-class="el-select--public"
+          @change="atInputChange"
+        >
+        </emq-select>
+        <!-- Number field -->
+        <el-input
+          v-else-if="row[item].type === 'number'"
+          type="number"
+          v-bind="row[item].$attrs"
+          v-model.number="row[row[item].key]"
+          @input="atInputChange"
+        >
+        </el-input>
+        <!-- String field -->
+        <el-input v-else v-bind="row[item].$attrs" v-model="row[row[item].key]" @input="atInputChange"> </el-input>
+      </template>
+    </el-table-column>
+    <el-table-column width="60px">
+      <span slot="header" class="btn" @click="addColumn">
+        {{ $t('rule.add') }}
+      </span>
+      <template slot-scope="{ row }">
+        <span class="btn" @click="deleteItem(row)">
+          {{ $t('rule.delete') }}
+        </span>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import EmqSelect from './EmqSelect'
+
+export default {
+  name: 'MulObjectEditor',
+
+  components: {EmqSelect},
+
+  model: {
+    prop: 'value',
+    event: 'update',
+  },
+
+  props: {
+    value: {
+      type: Array,
+      required: true,
+    },
+    notNull: {
+      type: Boolean,
+      default: false,
+    },
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      tableData: [],
+      headers: [],
+      oneRow: {},
+      defaultConfig: {},
+    }
+  },
+
+  computed: {},
+
+  created() {
+    this.initData()
+  },
+
+  methods: {
+    assignValue() {
+      if (this.value.length) {
+        for (let i = 0; i < this.value.length; i += 1) {
+          this.addColumn()
+        }
+        this.value.forEach((item) => {
+          const index = this.value.indexOf(item)
+          Object.assign(this.tableData[index], item)
+        })
+      }
+    },
+    initData() {
+      const { model } = this.data
+      // eslint-disable-next-line no-unused-vars
+      Object.entries(model).forEach(([k, v]) => {
+        const labelName = v.label
+        this.headers.push(labelName)
+        this.oneRow[labelName] = v
+        const { key, defaultValue } = v
+        this.oneRow[key] = defaultValue
+        this.defaultConfig[key] = defaultValue
+      })
+      this.assignValue()
+    },
+    labelHeads(h, { column }) {
+      return h('span', { class: 'table-head', style: { width: '100%' } }, [column.label])
+    },
+    atInputChange() {
+      const data = []
+      this.tableData.forEach((item) => {
+        const tempConfig = {}
+        Object.keys(this.defaultConfig).forEach((key) => {
+          tempConfig[key] = item[key]
+        })
+        data.push(tempConfig)
+      })
+      this.$emit('update', data)
+    },
+    deleteItem(row) {
+      this.tableData = this.tableData.filter(($) => $.key !== row.key)
+      this.atInputChange()
+    },
+    addColumn() {
+      const row = { ...this.oneRow }
+      row.key = Math.random().toString(16).slice(3)
+      this.tableData.push(row)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.key-and-value-editor {
+  font-size: 12px !important;
+
+  .el-input {
+    width: 100% !important;
+
+    &.key-input {
+      width: 120px !important;
+    }
+  }
+
+  .el-table__empty-block {
+    min-height: 40px;
+  }
+}
+</style>
